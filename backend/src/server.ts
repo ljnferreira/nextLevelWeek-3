c5998c89d685797b6faa944b8e1a87c2ee59af28
Lucas Ferreira <ljnferreira@gmail.com>
2020-10-27 19:49:39 -0300

    chore: basic configuraton of database

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,9 +1,29 @@
 import express from 'express';
 
+import './database/conection'
+
 const app = express();
 
-app.get('/users', (request, response)=>{
-  return response.json({message: 'hello world'})
+app.use(express.json());
+
+app.get('/users/:id', (request, response)=>{
+  return response.json({
+    params: request.params,
+    query: request.query,
+    body: request.body
+  })
+})
+
+app.post('/users', (request, response)=>{
+  return response.json({message: 'post'})
+})
+
+app.put('/users', (request, response)=>{
+  return response.json({message: 'put'})
+})
+
+app.delete('/users', (request, response)=>{
+  return response.json({message: 'delete'})
 })
 
 app.listen(3333);
