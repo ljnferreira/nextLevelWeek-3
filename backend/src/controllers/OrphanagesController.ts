1190f7ce157aef7148e18613505de99a4aeb40d0
Lucas Ferreira <ljnferreira@gmail.com>
2020-10-29 16:11:15 -0300

    feat: created 'controllers' folder and configured controller to create new data on Orphanages table

---
+++ backend/src/controllers/OrphanagesController.ts
@@ -0,0 +1,34 @@
+import { Request, Response } from "express";
+
+import { getRepository } from "typeorm";
+import Orphanage from '../models/Orphanages'
+
+export default {
+  async create(request: Request, response: Response ) {
+    const {
+      name,
+      latitude,
+      longitude,
+      about,
+      instructions,
+      opening_hours,
+      open_on_weekends
+    } = request.body;
+
+    const orphanageRepository = getRepository(Orphanage);
+
+    const orphanage = orphanageRepository.create({
+      name,
+      latitude,
+      longitude,
+      about,
+      instructions,
+      opening_hours,
+      open_on_weekends
+    });
+
+    await orphanageRepository.save(orphanage);
+
+    response.status(201).json(orphanage);
+  }
+}
