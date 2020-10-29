90957e5188d632444e9f9789fba720d6fed6abb2
Lucas Ferreira <ljnferreira@gmail.com>
2020-10-29 15:03:01 -0300

    fix: adding a new data form to the migration

--- backend/src/database/migrations/1603839505493-create_orphanages.ts
+++ backend/src/database/migrations/1603839505493-create_orphanages.ts
@@ -39,6 +39,10 @@ export class createOrphanages1603839505493 implements MigrationInterface {
           type: 'text',
         },
         {
+          name: 'opening_hours',
+          type: ' varchar',
+        },
+        {
           name: 'open_on_weekends',
           type: 'boolean',
           default: false,
